3b1f755d41b0bbdf732f95b113216df4c7b66bd6
helloxjs-prog <maria.helloisa@souunit.com.br>
2025-09-19 14:08:37 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+const options = document.querySelectorAll('.option');
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    const nextPage = option.getAttribute('data-next');
+    window.location.href = nextPage;
+  });
+});
+
---
+++ script3.js
@@ -0,0 +1,8 @@
+const options = document.querySelectorAll('.option');
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    const nextPage = option.getAttribute('data-next');
+    window.location.href = nextPage;
+  });
+});
